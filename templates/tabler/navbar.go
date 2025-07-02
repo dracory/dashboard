89d35b68249978114bc459e75cb6a0186134a007
Milan <lesichkovm@gmail.com>
2025-07-02 08:05:08 +0100

    feat: navbar updates

--- templates/tabler/navbar.go
+++ templates/tabler/navbar.go
@@ -1,8 +1,6 @@
 package tabler
 
 import (
-	"strings"
-
 	"github.com/dracory/dashboard/types"
 	"github.com/gouniverse/hb"
 	"github.com/samber/lo"
@@ -10,230 +8,121 @@ import (
 
 // topNavigation generates the top navigation bar
 func topNavigation(dashboard types.DashboardInterface) string {
-	header := navbarHeader()
-	container := navbarContainer()
-	rightNav := navbarRightContainer()
 
-	// Navigation menu variables
-	var (
-		navMenu *hb.Tag
-		nav     *hb.Tag
-	)
+	// Build main navigation
+	nav := buildNavigation(dashboard.GetMenuMainItems())
+	navMenu := navMenuContainer().
+		Child(hb.Div().
+			Class("d-flex flex-column flex-md-row flex-fill align-items-stretch align-items-md-center").
+			Child(nav))
 
-	// Initialize main navigation
-	navMenu = hb.Div().Class("collapse navbar-collapse").Attr("id", "navbar-menu")
-	nav = hb.Ul().Class("navbar-nav")
+	dropdownTheme := themedropdown(dashboard)
 
-	// Add main menu items
-	for _, item := range dashboard.GetMenuMainItems() {
-		if item.URL == "" && item.Title == "" {
-			// Divider
-			nav.Child(hb.Li().Class("nav-item").
-				Child(hb.Div().Class("dropdown-divider")))
-			continue
-		}
+	rightNav := navbarRightContainer().
+		ChildIfF(dropdownTheme != nil, func() hb.TagInterface {
+			dropdownTheme.AddClass("nav-link px-0")
+			return hb.Div().
+				Class("nav-item dropdown d-none d-md-flex me-3").
+				Child(dropdownTheme)
+		}).
+		Child(lo.TernaryF(dashboard.GetUser() != nil, func() *hb.Tag {
+			return navbarUserMenu(dashboard, dashboard.GetUser())
+		}, func() *hb.Tag {
+			return navbarLoginButton()
+		}))
 
-		li := hb.Li().Class("nav-item")
-		hasChildren := len(item.Children) > 0
+	// Assemble the navbar - rightNav will be on the far right
+	container := navbarContainer().
+		Child(navbarButtonToggler()).
+		Child(navbarBrand(dashboard)).
+		Child(navMenu).
+		// Add an auto-margin to push rightNav to the far right
+		Child(hb.Div().
+			Class("d-flex ms-auto").
+			Child(rightNav))
 
-		// Create link
-		link := hb.NewA().Class("nav-link")
-		if hasChildren {
-			li.AddClass("dropdown")
-			link.AddClass("dropdown-toggle")
-			link.Attr("data-bs-toggle", "dropdown")
-			link.Attr("role", "button")
-			link.Attr("aria-expanded", "false")
-		}
-
-		link.Attr("href", lo.Ternary(item.URL != "", item.URL, "#"))
-		if item.Target != "" {
-			link.Attr("target", item.Target)
-		}
-
-		if item.Icon != "" {
-			link.Child(hb.Raw(item.Icon)).AddClass("me-2")
-		}
-
-		link.Child(hb.Text(item.Title))
-
-		// Add dropdown menu if there are children
-		if hasChildren {
-			dropdownMenu := hb.NewDiv().Class("dropdown-menu")
-			for _, child := range item.Children {
-				if child.URL == "" && child.Title == "" {
-					dropdownMenu.Child(hb.NewDiv().Class("dropdown-divider"))
-					continue
-				}
-
-				childLink := hb.NewA().Class("dropdown-item").Href(child.URL)
-				if child.Target != "" {
-					childLink.Attr("target", child.Target)
-				}
-
-				if child.Icon != "" {
-					childLink.Child(hb.Raw(child.Icon)).AddClass("me-2")
-				}
-
-				childLink.Child(hb.Text(child.Title))
-				dropdownMenu.Child(childLink)
-			}
-			li.Child(link).Child(dropdownMenu)
-		} else {
-			li.Child(link)
-		}
-
-		nav.Child(li)
-	}
-
-	navMenu.Child(hb.NewDiv().Class("d-flex flex-column flex-md-row flex-fill align-items-stretch align-items-md-center").
-		Child(nav))
-
-	// Add theme toggles to right nav
-	if themeToggle := themetoggle(dashboard); themeToggle != nil {
-		themeToggle.AddClass("nav-link px-0")
-		rightNav.Child(hb.NewDiv().Class("nav-item").Child(themeToggle))
-	}
-
-	if themeDropdown := themedropdown(dashboard); themeDropdown != nil {
-		themeDropdown.AddClass("nav-link px-0")
-		rightNav.Child(hb.NewDiv().Class("nav-item dropdown d-none d-md-flex me-3").Child(themeDropdown))
-	}
-
-	// Add user menu to right nav
-	if user := dashboard.GetUser(); user != nil {
-		userMenu := hb.NewDiv().Class("nav-item dropdown")
-		userLink := hb.NewA().Class("nav-link d-flex lh-1 text-reset p-0")
-		userLink.Attr("data-bs-toggle", "dropdown")
-
-		// User avatar with initials
-		avatar := hb.NewSpan().Class("avatar avatar-sm")
-		if user.AvatarURL != "" {
-			avatar.Child(hb.Img(user.AvatarURL).Class("avatar"))
-		} else {
-			initials := ""
-			if len(user.FirstName) > 0 {
-				initials = string(strings.ToUpper(user.FirstName)[0])
-			}
-			avatar.Child(hb.NewSpan().Class("avatar-initials").Text(initials))
-		}
-
-		// User name
-		userInfo := hb.NewDiv().Class("d-none d-xl-block ps-2")
-		userInfo.Child(hb.NewDiv().Text(user.FirstName + " " + user.LastName))
-		if user.Email != "" {
-			userInfo.Child(hb.NewDiv().Class("mt-1 small text-muted").Text(user.Email))
-		}
-
-		userLink.Child(avatar).Child(userInfo)
-
-		// Dropdown menu
-		dropdownMenu := hb.NewDiv().Class("dropdown-menu dropdown-menu-end dropdown-menu-arrow")
-		for _, item := range dashboard.GetMenuUserItems() {
-			if item.URL == "" && item.Title == "" {
-				dropdownMenu.Child(hb.NewDiv().Class("dropdown-divider"))
-				continue
-			}
-			itemLink := hb.NewA().Class("dropdown-item").Href(item.URL)
-			if item.Target != "" {
-				itemLink.Attr("target", item.Target)
-			}
-			if item.Icon != "" {
-				itemLink.Child(hb.Raw(item.Icon)).AddClass("me-2")
-			}
-			itemLink.Child(hb.Text(item.Title))
-			dropdownMenu.Child(itemLink)
-		}
-
-		userMenu.Child(userLink).Child(dropdownMenu)
-		rightNav.Child(userMenu)
-	} else {
-		// Login button if no user
-		loginBtn := hb.NewA().Class("btn btn-primary")
-		loginBtn.Href("/login").Child(hb.Text("Sign in"))
-		rightNav.Child(loginBtn)
-	}
-
-	// Initialize main navigation
-	navMenu = hb.Div().Class("collapse navbar-collapse").Attr("id", "navbar-menu")
-	nav = hb.Ul().Class("navbar-nav")
-
-	// Add main menu items
-	for _, item := range dashboard.GetMenuMainItems() {
-		if item.URL == "" && item.Title == "" {
-			// Divider
-			nav.Child(hb.Li().Class("nav-item").
-				Child(hb.Div().Class("dropdown-divider")))
-			continue
-		}
-
-		li := hb.Li().Class("nav-item")
-		hasChildren := len(item.Children) > 0
-
-		// Create link
-		link := hb.NewA().Class("nav-link")
-		if hasChildren {
-			li.AddClass("dropdown")
-			link.AddClass("dropdown-toggle")
-			link.Attr("data-bs-toggle", "dropdown")
-			link.Attr("role", "button")
-			link.Attr("aria-expanded", "false")
-		}
-
-		link.Attr("href", lo.Ternary(item.URL != "", item.URL, "#"))
-		if item.Target != "" {
-			link.Attr("target", item.Target)
-		}
-
-		// Add icon if exists
-		if item.Icon != "" {
-			icon := hb.NewSpan().Class("nav-link-icon d-none d-md-inline-block me-1")
-			icon.Child(hb.Raw(item.Icon))
-			link.Child(icon)
-		}
-
-		// Add title
-		link.Child(hb.NewSpan().Class("nav-link-title").Text(item.Title))
-
-		li.Child(link)
-
-		// Add dropdown menu if has children
-		if hasChildren {
-			dropdown := hb.NewDiv().Class("dropdown-menu dropdown-menu-arrow")
-			for _, child := range item.Children {
-				if child.URL == "" && child.Title == "" {
-					dropdown.Child(hb.NewDiv().Class("dropdown-divider"))
-					continue
-				}
-				childLink := hb.NewA().Class("dropdown-item").Href(lo.Ternary(child.URL != "", child.URL, "#"))
-				if child.Target != "" {
-					childLink.Attr("target", child.Target)
-				}
-				if child.Icon != "" {
-					childLink.Child(hb.Raw(child.Icon)).AddClass("me-2")
-				}
-				childLink.Child(hb.Text(child.Title))
-				dropdown.Child(childLink)
-			}
-			li.Child(dropdown)
-		}
-
-		nav.Child(li)
-	}
-
-	navMenu.Child(hb.NewDiv().Class("d-flex flex-column flex-md-row flex-fill align-items-stretch align-items-md-center").
-		Child(nav))
-
-	container.Children([]hb.TagInterface{
-		navbarButtonToggler(),
-		navbarBrand(dashboard),
-		navMenu,
-		rightNav,
-	})
-
-	header.Child(container)
+	header := navbarHeader().
+		Child(container)
 
 	return header.ToHTML()
+}
+
+// navMenuContainer creates the main navigation menu container
+func navMenuContainer() *hb.Tag {
+	return hb.Div().
+		Class("collapse navbar-collapse").
+		Attr("id", "navbar-menu")
+}
+
+// navList creates the main navigation list
+func navList() *hb.Tag {
+	return hb.Ul().Class("navbar-nav")
+}
+
+// navDivider creates a divider for the navigation menu
+func navDivider() *hb.Tag {
+	return hb.Li().
+		Class("nav-item").
+		Child(hb.Div().Class("dropdown-divider"))
+}
+
+// navLink creates a navigation link
+func navLink(item types.MenuItem) *hb.Tag {
+	link := hb.A().
+		Class("nav-link").
+		Href(lo.Ternary(item.URL != "", item.URL, "#")).
+		Attr("target", item.Target)
+
+	if item.Icon != "" {
+		link.Child(hb.Raw(item.Icon)).AddClass("me-2")
+	}
+
+	return link.Child(hb.Text(item.Title))
+}
+
+// navDropdown creates a dropdown menu for navigation items with children
+func navDropdown(item types.MenuItem) *hb.Tag {
+	li := hb.Li().Class("nav-item dropdown")
+	link := navLink(item).
+		AddClass("dropdown-toggle").
+		Attr("data-bs-toggle", "dropdown").
+		Attr("role", "button").
+		Attr("aria-expanded", "false")
+
+	dropdownMenu := hb.Div().Class("dropdown-menu")
+	for _, child := range item.Children {
+		if child.URL == "" && child.Title == "" {
+			dropdownMenu.Child(navDivider())
+			continue
+		}
+		dropdownMenu.Child(navLink(child).AddClass("dropdown-item"))
+	}
+
+	return li.Child(link).Child(dropdownMenu)
+}
+
+// navItem creates a single navigation item
+func navItem(item types.MenuItem) *hb.Tag {
+	if item.URL == "" && item.Title == "" {
+		return navDivider()
+	}
+
+	if len(item.Children) > 0 {
+		return navDropdown(item)
+	}
+
+	return hb.Li().
+		Class("nav-item").
+		Child(navLink(item))
+}
+
+// buildNavigation builds the complete navigation menu
+func buildNavigation(menuItems []types.MenuItem) *hb.Tag {
+	nav := navList()
+	for _, item := range menuItems {
+		nav.Child(navItem(item))
+	}
+	return nav
 }
 
 // navbarHeader creates the main navbar container
@@ -274,6 +163,7 @@ func navbarRightContainer() *hb.Tag {
 	return hb.NewDiv().Class("navbar-nav flex-row order-md-last")
 }
 
+// navbarButtonToggler creates the mobile menu toggle button
 func navbarButtonToggler() *hb.Tag {
 	toggleBtn := hb.Button().
 		Class("navbar-toggler").
@@ -288,9 +178,72 @@ func navbarButtonToggler() *hb.Tag {
 	return toggleBtn
 }
 
+// navbarUserMenu creates the user menu dropdown
+func navbarUserMenu(dashboard types.DashboardInterface, user *types.User) *hb.Tag {
+	userMenu := hb.Div().Class("nav-item dropdown")
+	userLink := hb.A().
+		Class("nav-link d-flex lh-1 text-reset p-0").
+		Attr("data-bs-toggle", "dropdown")
+
+	// User avatar with initials
+	initials := ""
+	if len(user.FirstName) > 0 {
+		initials = string(user.FirstName[0])
+	}
+
+	avatar := hb.Span().
+		Class("avatar avatar-sm").
+		Child(hb.Span().
+			Class("avatar-initials").
+			Text(initials))
+
+	// User info
+	userInfo := hb.Div().Class("d-none d-xl-block ps-2")
+	userInfo.Child(hb.Div().Text(user.FirstName + " " + user.LastName))
+
+	if user.Email != "" {
+		userInfo.Child(hb.Div().
+			Class("mt-1 small text-muted").
+			Text(user.Email))
+	}
+
+	userLink.Children([]hb.TagInterface{avatar, userInfo})
+
+	// Dropdown menu
+	dropdownMenu := hb.Div().Class("dropdown-menu dropdown-menu-end dropdown-menu-arrow")
+	for _, item := range dashboard.GetMenuUserItems() {
+		if item.URL == "" && item.Title == "" {
+			dropdownMenu.Child(hb.Div().Class("dropdown-divider"))
+			continue
+		}
+
+		link := hb.A().
+			Class("dropdown-item").
+			Href(item.URL).
+			Attr("target", item.Target)
+
+		if item.Icon != "" {
+			link.Child(hb.Raw(item.Icon)).AddClass("me-2")
+		}
+
+		link.Child(hb.Text(item.Title))
+		dropdownMenu.Child(link)
+	}
+
+	return userMenu.Children([]hb.TagInterface{userLink, dropdownMenu})
+}
+
+// navbarLoginButton creates the sign-in button
+func navbarLoginButton() *hb.Tag {
+	return hb.A().
+		Class("btn btn-primary").
+		Href("/login").
+		Child(hb.Text("Sign in"))
+}
+
 // themetoggle generates the theme toggle button
 func themetoggle(dashboard types.DashboardInterface) *hb.Tag {
-	button := hb.NewButton().Class("btn btn-link px-2")
+	button := hb.Button().Class("btn btn-link px-2")
 	button.Child(hb.Raw(`<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" /></svg>`))
 	return button
 }
